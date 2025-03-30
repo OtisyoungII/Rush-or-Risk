4a190d2ef3625ecd9ad9d14a6c1f3f05a7edfa0e
Otis YII <oyoung21@gmail.com>
2025-03-30 15:33:02 -0400

    Initial Commit

---
+++ Rush or Risk/Rush_or_RiskApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rush_or_RiskApp.swift
+//  Rush or Risk
+//
+//  Created by Otis Young on 3/30/25.
+//
+
+import SwiftUI
+
+@main
+struct Rush_or_RiskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
